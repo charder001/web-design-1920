7adcced51d78daf1f95757123a47ea43c3091fc8
Charder001 <rick.chardet@hva.nl>
2020-04-22 12:01:28 +0200

    datepicker functional

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -3,12 +3,13 @@ var html = "";
 var blocksPerRow = 7;
 
 
-for (; i < 30; i++) {
-    html += `<a href="#" class='test'>link</a>`;
+for (; i < 31; i++) {
+    html += `<a href="${i}" class='test'>${i}</a>`;
 }
 
-$("#wrapper").append(html).children(":first").addClass("selected");
-document.querySelector(".test:first-of-type").focus()
+$("#wrapper").append(html).children(":nth-of-type(2)").addClass("selected");
+$("#wrapper").children(":first").addClass("hide")
+// document.querySelector(".test:first-of-type").focus()
 
 $("body").on("keydown", function (e) {
     var thisIndex = $(".selected").index();
@@ -27,7 +28,7 @@ $("body").on("keydown", function (e) {
     if (newIndex !== null) {
         $(".test").eq(newIndex).addClass("selected").siblings().removeClass("selected");
     }
-    if (thisIndex){
+    if (thisIndex) {
         console.log("test")
         $(".selected").focus()
     }
