09a14e93eb093430059abd915dfa312073d99682
Charder001 <rick.chardet@hva.nl>
2020-05-05 17:12:25 +0200

    added months

--- datepicker.js
+++ datepicker.js
@@ -1,15 +1,22 @@
 var i = 0;
 var html = "";
 var blocksPerRow = 7;
-
+var months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", 
+"Juli", "Augustus", "September", "Oktober", "November", "December"]
+var currentMonth = 4
+var monthHTML = `<a href="#" id='month'>${months[currentMonth]}</a>`
 
 for (; i < 31; i++) {
-    html += `<a href="hour.html" class='test'>${i}</a>`;
+    html += `<a href="https://www.ns.nl/reisplanner/#/?vertrek=Eindhoven%20Centraal&vertrektype=treinstation&aankomst=Amsterdam%20Centraal&aankomsttype=treinstation&type=vertrek&tijd=2020-05-05T16:55" class='test'>${i}</a>`;
 }
+
+var main = document.querySelector("main")
+main.insertAdjacentHTML("afterbegin", monthHTML)
+var monthField = document.querySelector("#month")
 
 document.querySelector("#wrapper").innerHTML = html
 document.querySelector(".test:nth-of-type(6)").classList.add("selected");
-document.querySelector(".test:first-of-type").classList.add("hide");
+document.querySelector(".test:nth-of-type(1)").classList.add("hide");
 document.querySelector(".test:nth-of-type(2)").focus()
 
 document.querySelector("body").addEventListener("keydown", function (e) {
@@ -21,7 +28,14 @@ document.querySelector("body").addEventListener("keydown", function (e) {
     if (e.keyCode === 75 || e.keyCode === 38) {
         // up
         newIndex = thisIndex - blocksPerRow;
-    } else if (e.keyCode === 74 || e.keyCode === 40) {
+    } else if (e.keyCode === 188 && currentMonth > 0){
+        currentMonth --
+        monthField.innerText = months[currentMonth]
+    } else if (e.keyCode === 190 && currentMonth < 11){
+        currentMonth ++
+        monthField.innerText = months[currentMonth]
+    } 
+    else if (e.keyCode === 74 || e.keyCode === 40) {
         // down
         newIndex = thisIndex + blocksPerRow;
     } else if (e.keyCode === 76 || e.keyCode === 39) {
